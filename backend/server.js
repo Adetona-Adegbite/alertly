6863e0faab1c39dd1abf10b61d32af43045ba8f9
Adetona-Adegbite <adetona67@gmail.com>
2025-03-20 13:57:39 +0100

    cert remove

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
 const sequelize = require("./config/database");
 const cors = require("cors");
-const https = require("https");
-const fs = require("fs");
+
 require("dotenv").config();
 
 const app = express();
@@ -18,22 +17,13 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-const options = {
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
-};
-
 sequelize
   .authenticate()
   .then(() => {
     const port = process.env.PORT || 3000;
-    https
-      .createServer(options, app)
-      .listen(port, () => console.log(`Server running on port ${port}`));
+
+    app.listen(port, () => console.log(`Server running on port ${port}`));
   })
   .catch((err) => {
     console.error("Failed to sync database:", err);
   });
-
-// Adegbite12..
-// 3306;
